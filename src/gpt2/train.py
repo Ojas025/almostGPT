fe5b3df75275867a069d319b2fb4707dc125d3d5
Ojas Deshpande <OjasD025@gmail.com>
2026-01-11 03:20:45 +0530

    add flash attention

--- src/gpt2/train.py
+++ src/gpt2/train.py
@@ -7,17 +7,26 @@ Changes in GPT-2 from the original transformer paper:
 """
 Notes:
     - Use of GeLU instead of ReLU to deal with dead activations, as GeLU always contributes a local gradient
+    - Includes Flash Attention for optimization
+    - Includes ugly number optimization
+    - Gradient clipping to a norm -> 1.0
+    - Learning rate schedule using cosine decay
 """
 
 from dataclasses import dataclass
 import torch
 import torch.nn as nn
-from transformers import GPT2LMHeadModel
+# from torch.utils.cpp_extension import load
 from rich.console import Console
+from rich.panel import Panel
 import tiktoken
+from time import time
 
 console = Console()
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+panel = Panel("ProbablyGPT", title="ProbablyGPT", title_align="center", style="bold white on red")
+console.print(panel)
 console.print(f"Using device: [yellow]{device}[/yellow]")
 
 def load_data():
@@ -35,57 +44,69 @@ class GPTConfig:
     n_head: int = 12 # number of heads
     n_embed: int = 768 # embedding dimension
 
-class AttentionHead(nn.Module):
-    """Masked/Causal Self Attention head"""
+# flash_attention_kernel = load(name="flash_attention", sources=["flash_attention_kernel.cu"])
 
-    def __init__(self, config: GPTConfig, dropout=0.2):
+# def flash_attention(q,k,v,scale):
+#     return flash_attention_kernel(q,k,v,scale)
+
+# class FlashAttention(torch.autograd.Function):
+#     """Flash attention implementation"""
+#     # TODO: implement CUDA kernel
+
+#     @staticmethod
+#     def forward(ctx, q, k, v, scaling):
+#         ctx.save_for_backward(q,k,v,scaling)
+#         output = flash_attention(q,k,v,scaling)
+#         return output
+    
+#     @staticmethod
+#     def backward(ctx, grad_outputs):
+#         q, k, v, scale = ctx.saved_tensors
+        
+#         grad_q, grad_k, grad_v = flash_attention_kernel(grad_outputs, q, k, v, scale)
+        
+#         return grad_q,grad_k,grad_v,None
+        
+class MaskedSelfAttention(nn.Module):
+    """Masked/Causal Self Attention"""
+
+    def __init__(self, config: GPTConfig, dropout=0.2, is_flash_attention=True):
         super().__init__()
         
+        self.is_flash_attention = is_flash_attention
+        self.config = config
         self.d_head = config.n_embed // config.n_head
+        self.scale = self.d_head ** -0.5
         
-        self.query = nn.Linear(config.n_embed, self.d_head, bias=False)
-        self.key = nn.Linear(config.n_embed, self.d_head, bias=False)
-        self.value = nn.Linear(config.n_embed, self.d_head, bias=False)
+        self.qkv_proj = nn.Linear(config.n_embed, 3*config.n_embed)
         
         self.register_buffer("tril", torch.tril(torch.ones(config.context_length, config.context_length)))
         
+        self.output_proj = nn.Linear(config.n_embed, config.n_embed)
         self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
         B, T, C = x.shape
         
-        Q = self.query(x) # [B,T,d_head]
-        K = self.key(x) # [B,T,d_head]
-        V = self.value(x) # [B,T,d_head]
+        # Q = self.query(x) # [B,T,d_head]
+        # K = self.key(x) # [B,T,d_head]
+        # V = self.value(x) # [B,T,d_head]
         
-        scale = self.d_head ** -0.5
-        W = (Q @ K.transpose(-2,-1)) * scale # [B,T,T]
+        qkv = self.qkv_proj(x) # [B,T,3*C]
+        qkv = qkv.view(B,T,self.config.n_head, 3, self.d_head) # [B,T,n_head,3,d_head]
+        Q,K,V = qkv[...,0,:], qkv[...,1,:], qkv[...,2,:] # [B,T,n_head,d_head]
         
-        W = W.masked_fill(self.tril[:T,:T] == 0, -1e9)
+        if self.is_flash_attention:
+            output = nn.functional.scaled_dot_product_attention(Q,K,V,is_causal=True,scale=self.scale)
+        else:
+            W = (Q @ K.transpose(-2,-1)) * self.scale # [B,T,n_head,T]
+            mask = self.tril[:T, :T].unsqueeze(0).unsqueeze(0)
+            W = W.masked_fill(mask == 0, torch.finfo(W.dtype).min)
+            W = torch.softmax(W, dim=-1) # apply row-wise
+            output = W @ V # [B,T,n_head,d_head]
         
-        W = torch.softmax(W, dim=-1) # apply row-wise
-        
-        output = W @ V # [B,T,d_head]
-        
-        return output
-
-class MaskedSelfAttention(nn.Module):
-    """Masked/Causal Self Attention"""
-
-    def __init__(self, config: GPTConfig, dropout=0.2):
-        super().__init__()
-        
-        self.attention_heads = nn.ModuleList([AttentionHead(config) for _ in range(config.n_head)]) # n_head for multi self-attention
-
-        self.W0 = nn.Linear(config.n_embed, config.n_embed) # weight matrix for reshaping the concatenated tensor
-        
-        self.dropout = nn.Dropout(dropout)
-    
-    def forward(self, x):
-        output = torch.cat([h(x) for h in self.attention_heads], dim=-1)
-        output = self.W0(self.dropout(output))
-        
-        return output
+        output = output.reshape(B,T,C)
+        return self.output_proj(output)
 
 class LayerNorm(nn.Module):
     """Layern Normalization"""
@@ -174,7 +195,8 @@ class GPT(nn.Module):
     
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
-            nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            std = 0.02 * (2 * self.config.n_layer) ** -0.5 # scale residuals
+            nn.init.normal_(module.weight, mean=0.0, std=std)
             
             if module.bias is not None:
                 nn.init.zeros_(module.bias)
@@ -243,26 +265,36 @@ class DataLoader:
 num_return_sequences = 5
 max_length = 50
      
+# torch.set_float32_matmul_precision("high")
+     
 # init model   
-model = GPT(GPTConfig())
+model = GPT(GPTConfig(vocab_size=50304)) # remove ugly number 50257 -> 50304
 model.eval()
 model.to(device)
+model.compile()
 
-console.print(f"[green]Initialized model[/green]")
+console.print(f"Initialized model")
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
-data_loader = DataLoader(4,32)
+data_loader = DataLoader(B=4,T=1024)
 
 for i in range(1, 51):
+    t0 = time()
     x,y = data_loader.next_batch()
     x,y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    logits, loss = model(x,y)
+    
+    with torch.autocast(device_type=device, dtype=torch.float16):
+        logits, loss = model(x,y)
     loss.backward()
     optimizer.step()
     
-    if i % 10 == 0:
-        console.print(f"Step {i}, Loss: {loss.item()}")
+    torch.cuda.synchronize()
+    t1 = time()
+    dt = (t1-t0)*1000
+    tokens_per_sec = (data_loader.B * data_loader.T) / (t1 - t0)
+    
+    print(f"Step {i} | Loss: {loss.item():.4f} | dt: {dt:.2f}ms | tokens/sec: {tokens_per_sec:.2f}")
 
 
 # console.print(f"[green]Tokenized input text[/green]")
